8d2140dbb9e095ebc1cce2110cf33d80f79911cf
Georgina Woodman <georginabeth94@gmail.com>
2024-09-15 22:19:58 +0100

    Initial Portfolio Webpage
    This is an initial portfolio webpage that was designed with the support of this youtube tutorial: https://www.youtube.com/watch?v=ldwlOzRvYOU&list=PLnTwGbyWY2W6XCIKlzQOpehpsXMOzUdXd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const title = document.getElementById("myTitle")
-
-title.addEventListener("click", event => {
-    title.classList.add("colour")
-    console.log(event)
-})
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
